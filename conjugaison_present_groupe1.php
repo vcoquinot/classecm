d9f2dbfe0f204f5a13c1008770de50df78ccd9bd
vcoquinot <vcoquinot@gmail.com>
2019-11-01 12:16:25 +0100

    conjug present page layout + added function to display form

--- conjugaison_present_groupe1.php
+++ conjugaison_present_groupe1.php
@@ -1,5 +1,5 @@
 <?php require_once "connexion_bdd.php";
-include "fonctions_francais.php"?>
+include "functions_french.php"?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,26 +8,20 @@ include "fonctions_francais.php"?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/classecm.css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <title>conjugaison : present premier groupe</title>
 </head>
 <body>
-  <div class="container-fluid">
-  <header class="d-flex align-items-center flex-column bg-success">
+  <div class="container">
+  <header class="d-flex align-items-center flex-column">
       <h1>Le présent du 1er groupe</h1>
-      <!-- Subheading -->
-      <p class="subheading font-weight-light mb-4">Il te suffit de prendre le radical du verbe et y ajouter les terminaisons du présent.</p>
   </header>
 
   <main>
     <section class="text-center mb-4 pt-2 pb-2 bg-info">
-      <h2>À TOI DE JOUER</h2>
-      <p class="subheading font-weight-light mb-2">Conjugue les verbes au présent</p>
+      <h2 class="play text-light">À TOI DE JOUER</h2>
+      <p class="subheading font-weight-light mb-2 text-light">Conjugue les verbes au présent</p>
       <!-- Image -->
-      <img class=" mb-4" src="img/avatars.svg" alt="">
-      <div class="row justify-content-center">
-        <p>------------<i class="fa fa-star"></i>------------</p>
-      </div>
+      <img class="mb-4" src="img/avatars.svg" alt="">
       
       <?php
       //BDD random question
@@ -35,28 +29,20 @@ include "fonctions_francais.php"?>
       $donnees = $question->fetch();
       ?>
 
-        <div class="form-group">
-          <form action="cible/conjugaison.php" method="get">        
-            <!--question display -->
-            <p class="font-weight-bold"><?php echo $donnees['intitule_question']; ?></p>
-            <?php
-            $_SESSION['id_question'] = $donnees['id_question'];
-            $_SESSION['intitule_question'] = $donnees['intitule_question'];
-            $question->closeCursor();?>
-            <!--Form answer-->
-          <?php afficherFormulaireConjugaison();?>
-          <input type="hidden" name="url" value="present_1.php">
-          </form>
-          </div>
-        </div>
-        <div class="row">
-
-
-
-      </main>
-
-
+      <div class="form-group">
+        <form action="cible_conjugation.php" method="get">        
+          <!--question display -->
+          <p class="font-weight-bold text-light"><?php echo $donnees['intitule_question']; ?></p>
+          <?php
+          $_SESSION['id_question'] = $donnees['id_question'];
+          $_SESSION['intitule_question'] = $donnees['intitule_question'];
+          $question->closeCursor();?>
+          <!--Form answer-->
+          <?php conjugationDisplay();?>
+          <input class="input border border-primary" type="hidden" name="url" value="present_1.php">
+        </form>
+      </div>
     </section>
-</div>
+  </main>
 </body>
 </html>
