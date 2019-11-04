e0de22b6c2ce3ebff8fbc979f234d2d8c2acbcbe
vcoquinot <vcoquinot@gmail.com>
2019-11-04 09:21:09 +0100

    changed links to present and future exercices

--- functions_french.php
+++ functions_french.php
@@ -1,11 +1,11 @@
-<?php 
-function conjugationDisplay(){
-  ?>
-  <div class="row justify-content-center mb-4">
-    <input type="text" class="text-center" name="reponse" placeholder="Ton verbe conjugué" aria-label="verbe conjugué">  
-  </div>
-  <div class="row justify-content-center">
-    <input class="bg-primary border-primary" type="submit" value=" Vérifier ma réponse" aria-label="valider">
-  </div>
-<?php
-}
+  <?php 
+  function conjugationDisplay(){
+    ?>
+    <div class="row justify-content-center mb-4">
+      <input type="text" class="text-center" name="reponse" placeholder="Ton verbe conjugué" aria-label="verbe conjugué">  
+    </div>
+    <div class="row justify-content-center">
+      <input class="bg-primary border-primary" type="submit" value=" Vérifier ma réponse" aria-label="valider">
+    </div>
+  <?php
+    }
