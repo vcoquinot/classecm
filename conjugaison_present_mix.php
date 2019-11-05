7f09d3d0d169f2f2e7fa0afc18c97960686eeae2
vcoquinot <vcoquinot@gmail.com>
2019-11-05 08:58:03 +0100

    indent correction

--- conjugaison_present_mix.php
+++ conjugaison_present_mix.php
@@ -21,33 +21,31 @@
         </header>
 
         <main class="quizz text-center mb-4 pt-2 pb-2 bg-info">
-            
-                <h2 class="play text-light">À TOI DE JOUER</h2>
-                <p class="subheading font-weight-light mb-2 text-light">Conjugue au présent</p>
-                <!-- Image -->
-                <img class="mb-4" src="img/avatar2.svg" alt="">
+            <h2 class="play text-light">À TOI DE JOUER</h2>
+            <p class="subheading font-weight-light mb-2 text-light">Conjugue au présent</p>
+            <!-- Image -->
+            <img class="mb-4" src="img/avatar2.svg" alt="">
                 
-                <?php
-                //BDD random question
-                $question = $bdd->query("SELECT intitule_question, id_question FROM question WHERE id_matiere = 2 && id_theme  BETWEEN '20' AND '22' AND '25' ORDER BY RAND() LIMIT 1");
-                $questionData = $question->fetch();
-                ?>
+            <?php
+            //BDD random question
+            $question = $bdd->query("SELECT intitule_question, id_question FROM question WHERE id_matiere = 2 && id_theme  BETWEEN '20' AND '22' AND '25' ORDER BY RAND() LIMIT 1");
+            $questionData = $question->fetch();
+            ?>
 
-                <div class="form-group">
-                    <form action="cible_conjugaison.php" method="get">        
-                    <!--question display -->
-                    <p class="font-weight-bold text-light"><?php echo $questionData['intitule_question']; ?></p>
-                    <?php
-                    $_SESSION['id_question'] = $questionData['id_question'];
-                    $_SESSION['intitule_question'] = $questionData['intitule_question'];
-                    $question->closeCursor();?>
+             <div class="form-group">
+                <form action="cible_conjugaison.php" method="get">        
+                <!--question display -->
+                <p class="font-weight-bold text-light"><?php echo $questionData['intitule_question']; ?></p>
+                <?php
+                $_SESSION['id_question'] = $questionData['id_question'];
+                $_SESSION['intitule_question'] = $questionData['intitule_question'];
+                $question->closeCursor();?>
                     
-                    <!--Form answer-->
-                    <?php conjugationForm();?>
-                    <input class="input border border-primary" type="hidden" name="url" value="conjugaison_present_mix.php">
-                    </form>
-                </div>
-       
+                <!--Form answer-->
+                <?php conjugationForm();?>
+                <input class="input border border-primary" type="hidden" name="url" value="conjugaison_present_mix.php">
+                </form>
+            </div>       
         </main>
     </div>
 </body>
