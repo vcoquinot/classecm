df4865975c4312f958f01d2edff74548b9a6e544
vcoquinot <vcoquinot@gmail.com>
2019-11-10 18:54:46 +0100

    correction bug question display in cible+erased php tags repetitions
    transformed $questionData['intitule_question'] into
    $_SESSION['intitule_question'] to get the same question display in cible + changed order of execution

--- conjugaison_present_mix.php
+++ conjugaison_present_mix.php
@@ -1,10 +1,12 @@
-<?php session_start();?>
-<?php include "fonctions.php";?>
-<?php include "fonctions_français.php";?>
-<?php require_once "connexion_bdd.php";?>
+<?php
+session_start();
+include "fonctions.php";
+include "fonctions_français.php";
+require_once "connexion_bdd.php";
+?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,10 +16,9 @@
 </head>
 
 <body>
-    <?php resetScoreAndQuestionNumber(); ?> 
     <div class="container">
         <header class="d-flex align-items-center flex-column">
-            <h1>Tous les types de verbes</h1>
+            <h1>Conjuguer au présent</h1>
         </header>
 
         <main class="quizz text-center mb-4 pt-2 pb-2 bg-info">
@@ -30,20 +31,20 @@
             //BDD random question
             $question = $bdd->query("SELECT intitule_question, id_question FROM question WHERE id_matiere = 2 && id_theme  BETWEEN '20' AND '22' AND '25' ORDER BY RAND() LIMIT 1");
             $questionData = $question->fetch();
-            ?>
+
+            $_SESSION['id_question'] = $questionData['id_question'];
+            $_SESSION['intitule_question'] = $questionData['intitule_question'];
+            $question->closeCursor();?>
+            
 
              <div class="form-group">
                 <form action="cible_conjugaison.php" method="get">        
-                <!--question display -->
-                <p class="font-weight-bold text-light"><?php echo $questionData['intitule_question']; ?></p>
-                <?php
-                $_SESSION['id_question'] = $questionData['id_question'];
-                $_SESSION['intitule_question'] = $questionData['intitule_question'];
-                $question->closeCursor();?>
+                    <!--question display -->
+                    <p class="font-weight-bold text-light"><?php echo $_SESSION['intitule_question']; ?></p>
                     
-                <!--Form answer-->
-                <?php conjugationForm();?>
-                <input class="input border border-primary" type="hidden" name="url" value="conjugaison_present_mix.php">
+                    <!--Form answer-->
+                    <?php conjugationForm();?>
+                    <input class="input border border-primary" type="hidden" name="url" value="conjugaison_present_mix.php">
                 </form>
             </div>       
         </main>
