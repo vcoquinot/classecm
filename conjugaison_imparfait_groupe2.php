db853a90ca8a933ec1b0467c2656f1458e2e2f90
vcoquinot <vcoquinot@gmail.com>
2019-11-10 18:59:59 +0100

    correction bug question display
    transformed $questionData['intitule_question'] into
    $_SESSION['intitule_question'] to get the same question  display in cible file

--- conjugaison_imparfait_groupe2.php
+++ conjugaison_imparfait_groupe2.php
@@ -12,13 +12,13 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/classecm.css">
-        <title>conjugaison : imparfait des verbes du deuxième groupe</title>
+        <title>L'imparfait des verbes du deuxième groupe</title>
     </head>
 
     <body> 
         <div class="container">
             <header class="d-flex align-items-center flex-column">
-                <h1>conjugaison : imparfait des verbes du deuxième groupe</h1>
+                <h1>L'imparfait des verbes du deuxième groupe</h1>
             </header>
 
             <main class="quizz text-center mb-4 pt-2 pb-2 bg-info">
@@ -31,16 +31,17 @@
             //BDD random question
             $question = $bdd->query("SELECT intitule_question, id_question FROM question WHERE id_matiere = 2 && id_theme = 18 ORDER BY RAND() LIMIT 1");
             $questionData = $question->fetch();
+
+            $_SESSION['id_question'] = $questionData['id_question'];
+            $_SESSION['intitule_question'] = $questionData['intitule_question'];
+            $question->closeCursor();?>
             ?>
 
             <div class="form-group">
                 <form action="cible_conjugaison.php" method="get">        
                     <!--question display -->
-                    <p class="font-weight-bold text-light"><?php echo $questionData['intitule_question']; ?></p>
-                    <?php
-                    $_SESSION['id_question'] = $questionData['id_question'];
-                    $_SESSION['intitule_question'] = $questionData['intitule_question'];
-                    $question->closeCursor();?>
+                    <p class="font-weight-bold text-light"><?php echo $_SESSION['intitule_question']; ?></p>
+ 
                     <!--Form answer-->
                     <?php conjugationForm();?>
                     <input class="input border border-primary" type="hidden" name="url" value="conjugaison_imparfait_groupe2.php">
