a89a5097099ae070baa548669fb6d99022445f43
vcoquinot <vcoquinot@gmail.com>
2019-11-10 19:00:48 +0100

    Correction of title

--- conjugaison_imparfait_mix.php
+++ conjugaison_imparfait_mix.php
@@ -12,18 +12,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/classecm.css">
-        <title>conjugaison à l' imparfait : tous les verbes</title>
+        <title>Conjuguer à l'imparfait</title>
     </head>
 
     <body> 
         <div class="container">
             <header class="d-flex align-items-center flex-column">
-                <h1>conjugaison à l' imparfait : tous les verbes</h1>
+                <h1>Conjuguer à l'imparfait</h1>
             </header>
 
             <main class="quizz text-center mb-4 pt-2 pb-2 bg-info">
             <h2 class="play text-light">À TOI DE JOUER</h2>
-            <p class="subheading font-weight-light mb-2 text-light">Conjugue les verbes à l' imparfait</p>
+            <p class="subheading font-weight-light mb-2 text-light">Conjugue les verbes à l'imparfait</p>
             <!-- Image -->
             <img src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortRound&accessoriesType=Blank&hairColor=Brown&facialHairType=Blank&clotheType=ShirtCrewNeck&clotheColor=Blue02&eyeType=Default&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Light' alt="">
 
